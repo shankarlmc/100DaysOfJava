007ad04c5241700269a8a934b32c3fc14f5fda0f
shankarlmc <shankarlmc012@gmail.com>
2022-12-13 13:14:36 -0500

    event handling going on ..

--- SampleFrame/SampleFrame.java
+++ SampleFrame/SampleFrame.java
@@ -1,35 +1,46 @@
 import javax.swing.*;
+import java.awt.event.*;
 
 public class SampleFrame {
     SampleFrame() {
         JFrame f = new JFrame("Registration Form");
-        // create labels
+
         JLabel firstName = new JLabel("First Name");
         firstName.setBounds(50, 50, 100, 30);
+
         JLabel lastName = new JLabel("Last Name");
         lastName.setBounds(50, 100, 100, 30);
+
         JLabel email = new JLabel("Email");
         email.setBounds(50, 150, 100, 30);
+
         JLabel password = new JLabel("Password");
         password.setBounds(50, 200, 100, 30);
+
         JLabel gender = new JLabel("Gender");
         gender.setBounds(50, 250, 100, 30);
+
         JLabel country = new JLabel("Country");
         country.setBounds(50, 300, 100, 30);
 
-        // create text fields
         JTextField tfirstName = new JTextField();
         tfirstName.setBounds(150, 50, 200, 30);
+
         JTextField tlastName = new JTextField();
         tlastName.setBounds(150, 100, 200, 30);
+
         JTextField temail = new JTextField();
         temail.setBounds(150, 150, 200, 30);
+
         JPasswordField tpassword = new JPasswordField();
         tpassword.setBounds(150, 200, 200, 30);
+
         JRadioButton male = new JRadioButton("Male");
         male.setBounds(150, 250, 100, 30);
+
         JRadioButton female = new JRadioButton("Female");
         female.setBounds(250, 250, 150, 30);
+
         String countries[] = { "Nepal", "India", "Aus", "U.S.A", "England", "Newzealand" };
         JComboBox cb = new JComboBox(countries);
         cb.setBounds(150, 300, 200, 30);
@@ -64,6 +75,12 @@ public class SampleFrame {
         // set frame visibility
         f.setVisible(true);
 
+        // add action listener to submit button
+        submit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(e);
+            }
+        });
     }
 
     public static void main(String[] args) {
