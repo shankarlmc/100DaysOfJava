63e07509c4a29e78acf3f327d6dd5bb1cf000b3b
shankarlmc <shankarlmc012@gmail.com>
2022-12-13 13:38:12 -0500

    displayed dialog box with data on submit

--- SampleFrame/SampleFrame.java
+++ SampleFrame/SampleFrame.java
@@ -2,57 +2,102 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class SampleFrame {
+
+    // Labels
+    private JLabel firstName;
+    private JLabel lastName;
+    private JLabel email;
+    private JLabel password;
+    private JLabel gender;
+    private JLabel country;
+    // text fields
+    private JTextField tfirstName;
+    private JTextField tlastName;
+    private JTextField temail;
+    private JPasswordField tpassword;
+
+    // radio buttons
+    private JRadioButton male;
+    private JRadioButton female;
+    private JRadioButton agree;
+    // combo box
+    private JComboBox cb;
+    // submit button
+    private JButton submit;
+    // constatnts
+    private String countries[] = { "Nepal", "India", "Aus", "U.S.A", "England", "Newzealand" };
+
     SampleFrame() {
         JFrame f = new JFrame("Registration Form");
 
-        JLabel firstName = new JLabel("First Name");
+        firstName = new JLabel("First Name");
         firstName.setBounds(50, 50, 100, 30);
 
-        JLabel lastName = new JLabel("Last Name");
+        lastName = new JLabel("Last Name");
         lastName.setBounds(50, 100, 100, 30);
 
-        JLabel email = new JLabel("Email");
+        email = new JLabel("Email");
         email.setBounds(50, 150, 100, 30);
 
-        JLabel password = new JLabel("Password");
+        password = new JLabel("Password");
         password.setBounds(50, 200, 100, 30);
 
-        JLabel gender = new JLabel("Gender");
+        gender = new JLabel("Gender");
         gender.setBounds(50, 250, 100, 30);
 
-        JLabel country = new JLabel("Country");
+        country = new JLabel("Country");
         country.setBounds(50, 300, 100, 30);
 
-        JTextField tfirstName = new JTextField();
+        tfirstName = new JTextField();
         tfirstName.setBounds(150, 50, 200, 30);
 
-        JTextField tlastName = new JTextField();
+        tlastName = new JTextField();
         tlastName.setBounds(150, 100, 200, 30);
 
-        JTextField temail = new JTextField();
+        temail = new JTextField();
         temail.setBounds(150, 150, 200, 30);
 
-        JPasswordField tpassword = new JPasswordField();
+        tpassword = new JPasswordField();
         tpassword.setBounds(150, 200, 200, 30);
 
-        JRadioButton male = new JRadioButton("Male");
+        male = new JRadioButton("Male");
         male.setBounds(150, 250, 100, 30);
 
-        JRadioButton female = new JRadioButton("Female");
+        female = new JRadioButton("Female");
         female.setBounds(250, 250, 150, 30);
 
-        String countries[] = { "Nepal", "India", "Aus", "U.S.A", "England", "Newzealand" };
-        JComboBox cb = new JComboBox(countries);
+        cb = new JComboBox(countries);
         cb.setBounds(150, 300, 200, 30);
 
-        // i agree radio button
-        JRadioButton agree = new JRadioButton("I agree to the terms and conditions.");
+        agree = new JRadioButton("I agree to the terms and conditions.");
         agree.setBounds(50, 350, 300, 30);
 
         // create submit button
-        JButton submit = new JButton("Submit");
+        submit = new JButton("Submit");
         submit.setBounds(250, 400, 100, 30);
-        // add labels and text fields to frame
+
+        // add action listener to submit button
+        submit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == submit) {
+                    String data1;
+                    String data = "First Name: " + tfirstName.getText();
+                    data += ", Last Name: " + tlastName.getText();
+                    data += ", Email: " + temail.getText();
+                    data += ", Country: " + cb.getItemAt(cb.getSelectedIndex());
+
+                    if (male.isSelected()) {
+                        data1 = "Gender: Male";
+                    } else {
+                        data1 = "Gender: Female";
+                    }
+                    data += ", " + data1;
+                    JOptionPane.showMessageDialog(f, data);
+                }
+
+            }
+        });
+
         f.add(firstName);
         f.add(tfirstName);
         f.add(lastName);
@@ -68,19 +113,11 @@ public class SampleFrame {
         f.add(cb);
         f.add(agree);
         f.add(submit);
-        // set frame size
-        f.setSize(500, 500);
-        // set layout to null
-        f.setLayout(null);
-        // set frame visibility
-        f.setVisible(true);
 
-        // add action listener to submit button
-        submit.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.out.println(e);
-            }
-        });
+        f.setSize(440, 500);
+        f.setResizable(false);
+        f.setLayout(null);
+        f.setVisible(true);
     }
 
     public static void main(String[] args) {
